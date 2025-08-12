0d43174c5411ad85c920ca4b7970e23daedf82d2
gulshanberwal <liteboy9999@gmail.com>
2025-08-12 10:08:02 +0530

    login changed

--- app/register/page.js
+++ app/register/page.js
@@ -131,7 +131,7 @@ export default function RegisterPage() {
           name="username"
           maxLength={18}
           type="text"
-          placeholder="Username"
+          placeholder="Username(cannot change it again)"
           required
           className="w-full border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-white rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
